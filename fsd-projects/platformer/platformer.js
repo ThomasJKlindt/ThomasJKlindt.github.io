836fd573bfd73edd393db3ea1af5f709cbef14c4
HueJRat <thomasjklindt@gmail.com>
2026-09-18 16:31:39 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(118, 0, 233)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,24 +33,28 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    //toggleGrid();
 
     // TODO 2 - Create Platforms
 
-
-
+    createPlatform(200, 600, 200, 50, "goldenrod");
+    createPlatform(200, 300, 200, 50, "goldenrod");
+    createPlatform(1000, 300, 200, 50, "goldenrod");
+    createPlatform(1000, 600, 200, 50, "goldenrod");
+    createPlatform(600, 450, 200, 50, "goldenrod");
 
     // TODO 3 - Create Collectables
 
+    createCollectable("diamond", 280, 250);
+    createCollectable("diamond", 1080, 250);
+    createCollectable("diamond", 680, 100);
 
-
-    
     // TODO 4 - Create Cannons
 
+    createCannon("top", 760, 2000);
+    createCannon("left", 200, 2000);
+    createCannon("right", 500, 2000);
 
-    
-    
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -3,7 +3,7 @@ const walkAcceleration = 2.5; // how much is added to the speed each frame
 const gravity = 0.5; // how much is subtracted from speedY each frame
 const friction = 1.5; // how much the player is slowed each frame
 const maxSpeed = 8; // maximum horizontal speed, not vertical
-const playerJumpStrength = 12; // this is subtracted from the speedY each jump
+const playerJumpStrength = 15; // this is subtracted from the speedY each jump
 const projectileSpeed = 8; // the speed of projectiles
 let shouldDrawGrid = false;
 let gridMade = false;
@@ -97,7 +97,10 @@ let animationDetails = {};
 
 var collectableList = {
   database: { image: "images/collectables/database.png" },
-  diamond: { image: "images/collectables/diamond-head.png" },
+  diamond: {
+    image:
+      "https://png.pngtree.com/png-clipart/20220314/original/pngtree-3d-rendering-diamond-decoration-png-image_7437732.png",
+  },
   grace: { image: "images/collectables/grace-head.png" },
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
